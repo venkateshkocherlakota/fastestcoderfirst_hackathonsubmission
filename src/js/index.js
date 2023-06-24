58262b07834e8a607c807a9afd83301971a81a14
Venkatesh Kocherlakota <venkateshk@carestack.com>
2023-06-24 15:11:35 +0530

    cleanup WIP 1

--- src/js/index.js
+++ src/js/index.js
@@ -59,16 +59,16 @@ function populateTable() {
     btnEdit.innerText = "Edit";
     btnEdit.className = "btn btn-primary btn-xs";
     const editBtnClickHandler = function () {
-        // set value of description
-        document.getElementById("edit-description").value = row.description;
-        // set value of amount
-        document.getElementById("edit-amount").value = row.amount;
-        // set value of type
-        document.getElementById("edit-" + row.type.toLowerCase()).checked = true;
-        // set value of id
-        document.getElementById("edit-id").value = row.id;
-        // show modal
-        $('#edit-modal').modal('show');
+      // set value of description
+      document.getElementById("edit-description").value = row.description;
+      // set value of amount
+      document.getElementById("edit-amount").value = row.amount;
+      // set value of type
+      document.getElementById("edit-" + row.type.toLowerCase()).checked = true;
+      // set value of id
+      document.getElementById("edit-id").value = row.id;
+      // show modal
+      $("#edit-modal").modal("show");
     };
     btnEdit.addEventListener("click", editBtnClickHandler);
     newCell.appendChild(btnEdit);
@@ -90,14 +90,6 @@ function populateTable() {
   updateBalance();
 }
 
-/*
-    This function sorts the financialEntries array in descending order based on the date property
-*/
-function sortItemsInDescendingOrder() {
-  financialEntries.sort((a, b) => {
-    return new Date(b.date) - new Date(a.date);
-  });
-}
 
 /*
     This function deletes a row from the table based on the id of the row
@@ -113,85 +105,101 @@ function deleteRowById(id) {
 }
 
 /*
+    This function sorts the financialEntries array in descending order based on the date property
+*/
+function sortItemsInDescendingOrder() {
+  financialEntries.sort((a, b) => {
+    return new Date(b.date) - new Date(a.date);
+  });
+}
+
+/*
     This function clears the table completely
 */
 function clearTable() {
   tableBody.innerHTML = "";
 }
 
-function addItemToTable(event) {
-    // get value of description
-    const description = document.getElementById("description").value;
-    // get value of amount
-    const amount = document.getElementById("amount").value;
-    // get value of type
-    const type = $('input[name=inlineRadioOptions]:checked').val();
 
-    // prevent adding row if description or amount is empty
-    if (description == "" || amount == "") {
-        return;
-    }
+/*
+    This function adds a new row to the table
+*/
+function addItemToTable() {
+  // get value of description
+  const description = document.getElementById("description").value;
+  // get value of amount
+  const amount = document.getElementById("amount").value;
+  // get value of type
+  const type = $("input[name=inlineRadioOptions]:checked").val();
 
-    // build row json object
-    const row = {
-        id: financialEntries.length + 1,
-        description: description,
-        amount: amount,
-        date: new Date().toISOString().slice(0, 10),
-        type: type
-    }
-    // add row to financialEntries array
-    financialEntries.push(row);
-    // populate table
-    populateTable();
+  // prevent adding row if description or amount is empty
+  if (description == "" || amount == "") {
+    return;
+  }
+
+  // build row json object
+  const row = {
+    id: financialEntries.length + 1,
+    description: description,
+    amount: amount,
+    date: new Date().toISOString().slice(0, 10),
+    type: type,
+  };
+  // add row to financialEntries array
+  financialEntries.push(row);
+  // populate table
+  populateTable();
 }
 
 /*
     This function updates the balance based on the data in the financialEntries array
 */
 function updateBalance() {
-    let balance = 0;
-    for (let row of financialEntries) {
-        if (row.type == "Income") {
-            balance += Number.parseFloat(row.amount);
-        } else {
-            balance -= Number.parseFloat(row.amount);
-        }
+  let balance = 0;
+  for (let row of financialEntries) {
+    if (row.type == "Income") {
+      balance += Number.parseFloat(row.amount);
+    } else {
+      balance -= Number.parseFloat(row.amount);
     }
-    document.getElementById("current-balance").innerHTML = ( balance >= 0 ? "" : "- ") + "$" + Math.abs(balance);
-    // change color of balance based on value
-    document.getElementById("current-balance").style.color = (balance > 0 ? "green" : balance == 0 ? "black" : "red");
+  }
+  document.getElementById("current-balance").innerHTML =
+    (balance >= 0 ? "" : "- ") + "$" + Math.abs(balance);
+  // change color of balance based on value
+  document.getElementById("current-balance").style.color =
+    balance > 0 ? "green" : balance == 0 ? "black" : "red";
 }
 
 /*
     Update row in financialEntries array based on id
 */
 function updateRowById() {
-    // get value of id
-    const id = document.getElementById("edit-id").value;
-    // get value of description
-    const description = document.getElementById("edit-description").value;
-    // get value of amount
-    const amount = document.getElementById("edit-amount").value;
-    // get value of type
-    const type = document.getElementById('edit-income').checked ? "Income" : "Expense";
-    for (let i = 0; i < financialEntries.length; i++) {
-        if (financialEntries[i].id == id) {
-            financialEntries[i] = {
-                id: id,
-                description: description,
-                amount: amount,
-                date: new Date().toISOString().slice(0, 10),
-                type: type
-            };
-            break;
-        }
+  // get value of id
+  const id = document.getElementById("edit-id").value;
+  // get value of description
+  const description = document.getElementById("edit-description").value;
+  // get value of amount
+  const amount = document.getElementById("edit-amount").value;
+  // get value of type
+  const type = document.getElementById("edit-income").checked
+    ? "Income"
+    : "Expense";
+  for (let i = 0; i < financialEntries.length; i++) {
+    if (financialEntries[i].id == id) {
+      financialEntries[i] = {
+        id: id,
+        description: description,
+        amount: amount,
+        date: new Date().toISOString().slice(0, 10),
+        type: type,
+      };
+      break;
     }
-    populateTable();
-    $('#edit-modal').modal('hide');
+  }
+  populateTable();
+  $("#edit-modal").modal("hide");
 }
 
-
 // Initial population of the table with seed data
-$('#edit-modal').modal('hide');
+$("#edit-modal").modal("hide");
 populateTable(financialEntries);
