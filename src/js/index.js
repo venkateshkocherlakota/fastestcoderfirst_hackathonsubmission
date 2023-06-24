c3f6c30f3c25a03ac9082399eefaedffae1d0423
Venkatesh Kocherlakota <venkateshk@carestack.com>
2023-06-24 14:48:20 +0530

    Working POC

--- src/js/index.js
+++ src/js/index.js
@@ -53,14 +53,37 @@ function populateTable() {
       newRow.appendChild(newCell);
     }
     let newCell = document.createElement("td");
+
+    // Add Edit Button
+    const btnEdit = document.createElement("BUTTON");
+    btnEdit.innerHTML = "Edit";
+    btnEdit.className = "btn btn-primary btn-xs";
+    const editBtnClickHandler = function () {
+        // set value of description
+        document.getElementById("edit-description").value = row.description;
+        // set value of amount
+        document.getElementById("edit-amount").value = row.amount;
+        // set value of type
+        document.getElementById("edit-" + row.type.toLowerCase()).checked = true;
+        // set value of id
+        document.getElementById("edit-id").value = row.id;
+        // show modal
+        $('#edit-modal').modal('show');
+    };
+    btnEdit.addEventListener("click", editBtnClickHandler);
+    newCell.appendChild(btnEdit);
+
+    // Add Delete Button
     const btnDelete = document.createElement("BUTTON");
     btnDelete.innerHTML = "Delete";
+    btnDelete.style.marginLeft = "5px";
     btnDelete.className = "btn btn-danger btn-xs";
     const removeBtnClickHandler = function () {
       deleteRowById(row.id);
     };
     btnDelete.addEventListener("click", removeBtnClickHandler);
     newCell.appendChild(btnDelete);
+
     newRow.appendChild(newCell);
     tableBody.appendChild(newRow);
   }
@@ -130,14 +153,45 @@ function updateBalance() {
     let balance = 0;
     for (let row of financialEntries) {
         if (row.type == "Income") {
-            balance += row.amount;
+            balance += Number.parseFloat(row.amount);
         } else {
-            balance -= row.amount;
+            balance -= Number.parseFloat(row.amount);
         }
     }
     document.getElementById("current-balance").innerHTML = ( balance >= 0 ? "" : "- ") + "$" + Math.abs(balance);
     // change color of balance based on value
     document.getElementById("current-balance").style.color = (balance > 0 ? "green" : balance == 0 ? "black" : "red");
 }
+
+/*
+    Update row in financialEntries array based on id
+*/
+function updateRowById() {
+    // get value of id
+    const id = document.getElementById("edit-id").value;
+    // get value of description
+    const description = document.getElementById("edit-description").value;
+    // get value of amount
+    const amount = document.getElementById("edit-amount").value;
+    // get value of type
+    const type = document.getElementById('edit-income').checked ? "Income" : "Expense";
+    for (let i = 0; i < financialEntries.length; i++) {
+        if (financialEntries[i].id == id) {
+            financialEntries[i] = {
+                id: id,
+                description: description,
+                amount: amount,
+                date: new Date().toISOString().slice(0, 10),
+                type: type
+            };
+            break;
+        }
+    }
+    populateTable();
+    $('#edit-modal').modal('hide');
+}
+
+
 // Initial population of the table with seed data
+$('#edit-modal').modal('hide');
 populateTable(financialEntries);
